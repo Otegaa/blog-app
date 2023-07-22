d153253f9276b305b181cdb31f250872d198440c
otegaa <ekiugbotega@gmail.com>
2023-07-22 19:04:05 +0200

    Implement forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = @post.author
   end
+
+  def new
+    @post = Post.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    else
+      puts @user
+      puts @post.errors.full_messages
+      flash.now[:errors] = 'Invalid post!'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
